11f9bc6013452755f3ad8939ed3cb04d325dbbc8
yuvarajmadineni <yuvarajuv49@gmail.com>
2024-10-04 00:55:24 +0530

    respond with body

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,26 @@ fn main() {
             Ok(mut _stream) => {
                 let buf_reader = BufReader::new(&mut _stream);
                 let request_line = buf_reader.lines().next().unwrap().unwrap();
-                let response = match request_line.as_str() {
-                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
-                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
-                };
+                let req: Vec<_> = request_line.split(" ").collect();
+                let req_url = req[1];
+                let response;
+                let mut dynamic_path = "";
+                let mut dynamic_path_len = 0;
+
+                let path = req_url.split("/").collect::<Vec<_>>();
+
+                if path.len() >= 3 {
+                    dynamic_path = path[2];
+                    dynamic_path_len = dynamic_path.len();
+                }
+                if req_url.starts_with("/etc") {
+                    response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", dynamic_path_len, dynamic_path)
+                } else {
+                    response = match request_line.as_str() {
+                        "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+                        _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+                    };
+                }
                 _stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
