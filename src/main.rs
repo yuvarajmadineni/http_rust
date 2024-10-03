a6a634c6111cf4a35f9df405b7434ee11d175edb
yuvarajmadineni <yuvarajuv49@gmail.com>
2024-10-03 23:33:43 +0530

    extract url path and respond

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                let buf_reader = BufReader::new(&mut _stream);
+                let request_line = buf_reader.lines().next().unwrap().unwrap();
+                let response = match request_line.as_str() {
+                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n",
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
                 _stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
