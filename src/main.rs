7ac0ef772123795dc2ca6539f01655a96be1c994
yuvarajmadineni <yuvarajuv49@gmail.com>
2024-10-03 13:29:07 +0530

    initial http response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -11,8 +12,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                let response = "HTTP/1.1 200 OK\r\n\r\n";
+                _stream.write_all(response.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
